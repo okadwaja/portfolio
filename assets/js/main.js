8823ae54e08457d5fe88897e21c879e32294bf2c
okadwaja <okadwaja99@gmail.com>
2025-05-19 23:43:04 +0800

    feat: create developer portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,6 +55,43 @@ setInterval(changeWord, interval);
   }
 
   /**
+ * Custom handler for portfolio dropdown links
+ */
+  on('click', '.dropdown a', function(e) {
+    let section = select(this.hash)
+    if (section) {
+      e.preventDefault()
+
+      let header = select('#header')
+      let sections = select('section', true)
+      let navlinks = select('#navbar .nav-link', true)
+
+      navlinks.forEach((item) => {
+        item.classList.remove('active')
+      })
+
+      this.classList.add('active')
+
+      if (!header.classList.contains('header-top')) {
+        header.classList.add('header-top')
+        setTimeout(function() {
+          sections.forEach((item) => {
+            item.classList.remove('section-show')
+          })
+          section.classList.add('section-show')
+        }, 350)
+      } else {
+        sections.forEach((item) => {
+          item.classList.remove('section-show')
+        })
+        section.classList.add('section-show')
+      }
+
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
    * Scrool with ofset on links with a class name .scrollto
    */
   on('click', '#navbar .nav-link', function(e) {
